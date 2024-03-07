6ed0120ac153e08a650488ebb736de7ce8302169
vignesh <vigneshcv18@gmail.com>
2024-03-07 09:19:16 +0530

    used for the test

---
+++ pratice.py
@@ -0,0 +1,72 @@
+print ("hello vikcy")
+
+
+# this is command
+""" thisi salso command
+for the testing thank you"""
+
+"""x=10
+print(x)
+x=5
+print(x)
+a=20
+b=30
+print(a-b)"""
+
+a=False
+print(type(a))
+
+b= float(20)
+print(b, type(b))
+
+c= int(12424.5483432)
+print(c, type(c))
+
+#type
+a=10
+print(a, type(a))
+
+#typecasting
+
+t=int(10.28383)
+print(t, type(t))
+
+
+#input method
+
+"""print("itha kodu: ")
+a=input()
+print("a=", a)
+
+print("ithaium nethan ketkanum: ")
+b=input()
+print("b=", b)
+
+c= a+b
+print("final ithan ne kodutha: ", c)"""
+
+#in above kodutha code ku answer 1020 varuthu, add pannailla, so add pandrathuku
+
+print("itha kodu: ")
+a=int(input())
+print("a=", a)
+
+print("ithaium nethan ketkanum: ")
+b=int(input())
+print("b=", b)
+
+c= a+b
+print("final ithan ne kodutha: ", c)
+
+#trying float 
+
+print("itha kodu: ")
+a=float(input())
+print("a=", a)
+
+print("ithaium nethan ketkanum: ")
+b=float(input())
+print("b=", b)
+
+c= a+b
+print("final ithan ne kodutha: ", c)
