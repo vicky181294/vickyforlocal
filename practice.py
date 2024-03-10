e6c69b142a52dd0000c1387440282933358f3bc7
vignesh <vigneshcv18@gmail.com>
2024-03-10 11:35:31 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,34 @@
+"""print ("give your first numbe")
+a = int(input())
+print ("give second number")
+b= int(input())
+if a > b:
+    greatest = a
+elif b > a:
+    greatest = b
+else:
+    greatest = "both number are equal"
+    
+print( "this is greatest one.", greatest)
+"""
+
+"""# Iterate through numbers from 1 to 100
+for i in range(1, 101):
+    # Check if the number is divisible by 3
+    if i % 3 == 0:
+        # Print the number if divisible by 3
+        print(i)"""
+        
+"""# Initialize a counter variable
+count = 0
+
+# Iterate through numbers from 1 to 100
+for i in range(1, 101):
+    # Check if the number is divisible by 3
+    if i % 3 == 0:
+        # Increment the counter
+        count += 1
+
+# Print the count
+print("Count of numbers divisible by 3 from 1 to 100:", count)
+"""
